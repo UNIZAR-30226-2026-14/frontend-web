6199f1cc176a4322037edbfbc4d520041df0f0d2
Javier <897154@gmail.es>
2026-03-02 12:40:27 +0100

    Añadidas tarjetas con los modos de juego

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,7 +18,9 @@ function Home({onStart}) {
                     <circle className='profile' cx={0} cy={-5} r={40} onClick={toggleProfile}/>
                     <circle className='level' cx={35} cy={-30} r={15}/>
                 </svg>
+
                 <h1 className='title'>RUMMIPLUS</h1>
+
                 <svg width={100} height={100} viewBox='-50 -50 100 100'>
                     <circle className='friends' cx={0} cy={-5} r={40} onClick={toggleFriends}/>
                     <g className='friends-icon' onClick={toggleFriends}>
@@ -26,29 +28,49 @@ function Home({onStart}) {
                         <path d="M -25 25 C -25 -17 25 -17 25 25 Z"/>
                     </g>
                 </svg>
+
                 <svg width={150} height={100} viewBox='0 0 200 50'>
                     <rect className='shop' x={0} y={-15} width={200} height={50} rx={12}/>
                     <path className d="M 0 0 L 0 25"/>
                 </svg>
             </div>
+
             {isProfileOpen && (
                 <div className="profile-overlay">
                     <button className='close-button' onClick={toggleProfile}>X</button>
                 </div>
             )}
+
             {isFriendsOpen && (
                 <div>
                     <FriendsList onClose={toggleFriends}/>
                 </div>
             )}
-            <svg width={800} height={1000}>
-                <g>
-                    <rect className='regular-match' x={50} y={25} width={250} height={400} rx={10} onClick={onStart}/>
-                    
-                    <rect className='tile' x={80} y={50} width={90} height={150}/>
-                    <rect className='tile' x={80} y={150} width={90} height={150} fill='magenta'/>
-                </g>
-            </svg>
+
+            <div className='gamemodes'>
+
+                {/* Modo normal */}
+                <div className='gamemode-card' onClick={onStart}>
+                    <svg width={300} height={450}>
+                        <g>
+                            <rect className='regular-background' x={50} y={25} width={250} height={400} rx={10}/>
+                            <text className='gamemode-title' x={100} y={75}>Modo Clásico</text>
+                            <rect className='tile-bottom' x={120} y={125} width={150} height={200} rx={8}/>
+                            <rect className='tile-top' x={120} y={125} width={150} height={200} rx={8}/>
+                        </g>
+                    </svg>
+                </div>
+
+                {/* Modo con power-ups */}
+                <div className='gamemode-card'>
+                    <svg width={300} height={450}>
+                        <g>
+                            <rect className='enhanced-background' x={50} y={25} width={250} height={400} rx={10}/>
+                            <text className='gamemode-title' x={100} y={75}>Modo Arcade</text>
+                        </g>
+                    </svg>
+                </div>
+            </div>
         </div>
     )
 }
