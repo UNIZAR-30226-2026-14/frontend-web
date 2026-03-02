bc46d8b006df319a34aa0e7d75508b7cce2d37da
Javier <897154@gmail.es>
2026-03-02 17:31:51 +0100

    Añadido el + de la tienda

--- src/Home.jsx
+++ src/Home.jsx
@@ -13,14 +13,20 @@ function Home({onStart}) {
 
     return (
         <div className='home-screen'>
+
+            {/* Menú de arriba */}
             <div className='top-menu'>
+
+                {/* Perfil */}
                 <svg width={100} height={100} viewBox='-50 -50 100 100'>
                     <circle className='profile' cx={0} cy={-5} r={40} onClick={toggleProfile}/>
                     <circle className='level' cx={35} cy={-30} r={15}/>
                 </svg>
 
+                {/* Título */}
                 <h1 className='title'>RUMMIPLUS</h1>
 
+                {/* Amigos */}
                 <svg width={100} height={100} viewBox='-50 -50 100 100'>
                     <circle className='friends' cx={0} cy={-5} r={40} onClick={toggleFriends}/>
                     <g className='friends-icon' onClick={toggleFriends}>
@@ -29,24 +35,28 @@ function Home({onStart}) {
                     </g>
                 </svg>
 
+                {/* Tienda */}
                 <svg width={150} height={100} viewBox='0 0 200 50'>
                     <rect className='shop' x={0} y={-15} width={200} height={50} rx={12}/>
-                    <path className d="M 0 0 L 0 25"/>
+                    <path className='shop-add' d="M 157 11 L 183 11 M 170 -2 L 170 24"/>
                 </svg>
             </div>
 
+            {/* Pop-up del perfil */}
             {isProfileOpen && (
                 <div className="profile-overlay">
                     <button className='close-button' onClick={toggleProfile}>X</button>
                 </div>
             )}
 
+            {/* Pop-up de los amigos */}
             {isFriendsOpen && (
                 <div>
                     <FriendsList onClose={toggleFriends}/>
                 </div>
             )}
 
+            {/* Modos de juego */}
             <div className='gamemodes'>
 
                 {/* Modo normal */}
